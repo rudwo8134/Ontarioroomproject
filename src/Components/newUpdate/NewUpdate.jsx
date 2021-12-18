d8c9a7da94e99ea80f64425040f29bdbc06cdf71
Eric Shin <92757570+eshin-at-speer@users.noreply.github.com>
2021-12-18 17:39:24 +0900

    fix the issue

--- src/Components/newUpdate/NewUpdate.jsx
+++ src/Components/newUpdate/NewUpdate.jsx
@@ -6,17 +6,25 @@ import { createStructuredSelector } from 'reselect';
 import { selectitems } from '../../Redux/Rentcondo/rentcondo.selector';
 import { connect } from 'react-redux';
 import { rentcondoreadstart } from '../../Redux/Rentcondo/rentcondo.action';
+import {
+  BsFillCaretRightFill,
+} from 'react-icons/bs';
+import {GoPrimitiveDot} from 'react-icons/go'
 
 const NewupdateWrapper = styled.div`
   width: 100vw;
-  height: 627px;
+  height: 420px;
+
   .headerContainer {
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-bottom: 48px;
+    justify-content: center;
+    margin: 50px auto;
+
     .updatename {
+      color: ${CommonStyles.color.Primary};
       font-size: ${CommonStyles.fontSize.ExtraLarge1};
       font-weight: ${CommonStyles.bold.LittleBold};
       text-align: 'center';
@@ -24,43 +32,58 @@ const NewupdateWrapper = styled.div`
     .line {
       background-color: ${CommonStyles.color.Primary};
       width: 75px;
-      height: 4px;
+      height: 1px;
       border-radius: 30px;
       margin-top: -20px;
       margin-bottom: 16px;
     }
   }
   .cardContainer {
+    position: relative;
     display: flex;
+    justify-content: center;
     margin: 0 30px;
+
     .rentCard {
-      width: 350px;
-      height: 350px;
+      width: 300px;
+      height: 290px;
       margin-right: 30px;
       position: relative;
       cursor: pointer;
+      :last-child{
+        margin-right: 0px;
+      }
     }
     .textcontainer {
       position: absolute;
-      left: 10%;
-      bottom: 15%;
+      bottom: 0;
+      width: 100%;
+      height: 150px;
+      background-color: rgba(35, 31, 32, 0.4);
+      padding-left: 25px;
+      margin-bottom:1px;
+
       .address {
         font-size: 22;
-        font-weight: ${CommonStyles.bold.LittleBold};
+        font-weight: ${CommonStyles.bold.Medium};
         color: ${CommonStyles.color.White};
       }
       .price {
         font-size: 22;
-        font-weight: ${CommonStyles.bold.LittleBold};
+        font-weight: ${CommonStyles.bold.Medium};
         color: ${CommonStyles.color.Primary};
+        padding: 0px auto;
       }
       span {
-        font-weight: 300;
+        font-weight: 200;
         font-size: 14px;
         color: ${CommonStyles.color.White};
         text-decoration-line: underline;
         text-underline-position: under;
-        text-decoration-thickness: 2px;
+        text-decoration-thickness: 1.5px;
+        position: absolute;
+        right: 35px;
+        bottom: 20px;
       }
     }
     .imgfile {
@@ -70,49 +93,110 @@ const NewupdateWrapper = styled.div`
   }
 `;
 
+const Arrowleft = styled(BsFillCaretRightFill)`
+  transform: rotate(-180deg);
+  position: absolute;
+  top: 50%;
+  left: 32px;
+  font-size: 3rem;
+  color: ${CommonStyles.color.Dark};
+  cursor: pointer;
+  user-select: none;
+  z-index: 10;
+  transition: 0.5s all ease-in-out;
+  :hover {
+    color: ${CommonStyles.color.Primary};
+  }
+`;
+const Arrowright = styled(BsFillCaretRightFill)`
+  position: absolute;
+  top: 50%;
+  right: 32px;
+  font-size: 3rem;
+  color: ${CommonStyles.color.Dark};
+  cursor: pointer;
+  user-select: none;
+  z-index: 10;
+  transition: 0.5s all ease-in-out;
+  :hover {
+    color: ${CommonStyles.color.Primary};
+  }
+`;
+
+
+
+const Dotfolder = styled.div`
+  width: 100%;
+  height: 10px;
+  display: flex;
+  justify-content: center;
+  margin: 20px auto;
+`;
+
 const NewUpdate = ({ rooms, readStart }) => {
   const [loading, setLoading] = useState(false);
+  
   useEffect(() => {
     setLoading(true);
     readStart();
     setLoading(false);
   }, [readStart]);
+  
+  const [currentImg, setCurrentImg] = useState(0);
+  const length = rooms?.image?.length;
 
+  const nextSlide = () => {
+    setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1);
+  };
+  const previousSlide = () => {
+    setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1);
+  };
 
+  
   if (loading) {
     return <h1>loading.....</h1>;
   }
+
   return (
+
     <NewupdateWrapper>
       <div className="headerContainer">
         <h1 className="updatename">최신 게시물</h1>
         <div className="line"></div>
         <span>최근 7일 이전 집구하기 목록 </span>
       </div>
+      
       <div className="cardContainer">
-        {rooms &&
-          rooms?.slice(0, 5)?.map((data) => {
-            return (
-              <div key={data.id} className="rentCard">
-                <div className="textcontainer">
-                  <h2 className="address">{data?.address?.Formattedaddress}</h2>
-                  <h3 className="price"> {data.monthlyfee}</h3>
-                  <span>Load more</span>
-                </div>
-                <img
-                  className="imgfile"
-                  src={data?.image[0] ? data?.image[0] : data?.image[1]}
-                  alt="img"
-                  onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.src = `${Background}`;
-                  }}
-                />
+
+          <Arrowleft onClick={previousSlide} />
+          <Arrowright onClick={nextSlide} />
+          {rooms &&
+            rooms?.slice(0, 4)?.map((data) => {
+              return (
+                <div key={data.id} className="rentCard">
+                  <div className="textcontainer">
+                    <h2 className="address">{data?.address?.Formattedaddress?.split(',')[0]}</h2>
+                    <h3 className="price"> ${data.monthlyfee.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h3>
+                    <span>Learn more</span>
+                  </div>
+                  <img
+                    className="imgfile"
+                    src={data?.image[0] ? data?.image[0] : data?.image[1]}
+                    alt="img"
+                    onError={(e) => {
+                      e.target.onerror = null;
+                      e.target.src = `${Background}`;
+                    }}/>
+         
               </div>
             );
-          })}
+          })} 
+
       </div>
     </NewupdateWrapper>
+
+    
+  
   );
 };
 
